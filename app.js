95c5b182414f9ef57d47becf77077eb4115a0ea7
DiegoJimenez1 <48490090+DiegoJimenez1@users.noreply.github.com>
2022-04-12 12:00:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const axios = require('axios').default;
 var mysql = require('mysql');
+const PORT = process.env.PORT || 3050;
+const express = require('express');
+const bodyParser = require('body-parser');
+
 var precio,hora,unidades,fecha;
 
+const app = express();
+app.use(bodyParser.json());
+
+
+
+//----------------------------------leer datos api -------------------------------------------//
 
 let url = 'https://api.preciodelaluz.org/v1/prices/all?zone=PCB'
 axios.get(url,{
@@ -10,7 +20,6 @@ axios.get(url,{
 .then((response) => {
     //console.log(response);
     
-   
     var conexion = mysql.createConnection({
         host:"localhost",
         database:"prueba",
@@ -31,83 +40,165 @@ axios.get(url,{
    let aux = Object.values(response.data);
 
    var values_precio = [];
-   var sql = "INSERT INTO precios (precio, hora, unidades, fecha) VALUES ?";
-   var sql_= "SELECT * FROM precios where fecha = ?";
+   var date1 = (new Date()).toISOString().split('T')[0];
+   console.log(aux[0].date);
+
+   var sql = "INSERT INTO precios (precio, hora, unidades, Fecha_string) VALUES ?";
+   var sql_= `SELECT * FROM precios WHERE Fecha_string = "${aux[0].date}"`;
    var Flag = true;
 
-   var date = (new Date()).toISOString().split('T')[0];
-
-  /* 
-   conexion.query(sql_, [date], function(err, rows, fields) {
-    if (err) throw err;
-    if (rows.length) {
-      rows.forEach(function(row) {
-        console.log("existe");
-        Flag = false;
-      });
-    } else {
-      console.log('There were no results.');
-      Flag = true;
-    }
-  });
+   
   
-console.log(Flag);
+   conexion.query(sql_,(error,result)=>{
+    if (error) throw error;
 
-*/
-    if(Flag == true){
+    if ( result.length > 0){
+        //console.log(result);
+        console.log("si que existe");
+        Flag == true;
+    }else{
+        console.log("no existe");
+        Flag == false;
+    }
+});
+  
+
+
+/*
+   
         for ( let i = 0; i < Object.values(response.data).length ; i++){
             
         
             precio = aux[i].price;
             hora = aux[i].hour;
             unidades = aux[i].units;
-            fecha = aux[i].date;
+            fecha = aux[i].date1;
             
             console.log(hora);
             console.log(fecha);
             console.log(precio);
 
             //var date = (new Date()).toISOString().split('T')[0];
-            console.log(date);
-
-                
+            console.log(date1);
             
-
-
-            var value_=[[precio,hora,unidades,date]];
+            var value_=[[precio,hora,unidades,fecha]];
             conexion.query(sql, [value_], function (err, result) {
                 if (err) throw err;
                 console.log("Number of records inserted: " + result.affectedRows);
             });
-
-
-            //conexion.query(INSERT INTO precios (precio, hora, unidades, fecha) VALUES ('$precio',));
-
             
-
-
-            //[precio,hora,unidades,fecha]
 
         } 
 
-    }else{
-        console.log("ya esta en la bbdd");
-    }
-            
    
- 
-            
-    
     //conexion.query("INSERT INTO precios (precio, hora, unidades, fecha) VALUES (2.3,$hora,$unidades,'2022-02-03')");
-    
+    */
     conexion.end();
 
-
+   
 
 })
 .catch(err => {
     console.log(err);
 })
+
+//--------------------- BBDD ------------------------------------------//
+
+var conexion = mysql.createConnection({
+    host:"localhost",
+    database:"prueba",
+    user:"admin",
+    password:"admin2Pass=",
+
+});
+
+var conectar = function conectarBBDD(){
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('CONEXION EXITOSA');
+    }
+});
+
+}
+
+//---------------------- End points ------------------------------------//
+
+app.listen(PORT, () => console.log(`run on port ${PORT}`));
+
+app.get('/',(req,res) => {
+    res.send('welcome to my api');
+});
+
+    //----------- manejo usuarios---------//
+
+    app.get('/infoUser',(req,res) => {
+        const sql = `SELECT * FROM Inicio_sesion `;
+
+        var aux2= "Sep 09 2020 00:00:00";
+        var aux = new Date("Sep 09 2020 00:00:00");
+        console.log(aux);
+        
+
+
+        const sql2 = `SELECT * FROM Inicio_sesion WHERE fecha = ${aux2}`;
+
+       conexion.query(sql2,(error,result)=>{
+           if (error) throw error;
+
+           if ( result.length > 0){
+               res.json(result);
+           }else{
+               res.send('no result');
+           }
+       });
+    });
+
+    app.post('/newUser',(req,res) => {
+        res.send('newUser');
+    });
+
+    app.post('/modificarUser',(req,res) => {
+        res.send('modificarUser');
+    });
+
+
+
+    //----------------- precios ----------------------------//
+
+    app.get('/preciosLuzHoras/:fecha',(req,res) => {
+
+        const {fecha}=req.params;
+        console.log(fecha);
+        /*
+        var proposedDate = fecha + "T22:00:00.000Z";
+        console.log(proposedDate);
+        var aux = new Date(proposedDate);
+        console.log(aux);
+        */
+        const sql = `SELECT * FROM precios WHERE Fecha_string = "${fecha}"`;
+
+       conexion.query(sql,(error,result)=>{
+           if (error) throw error;
+
+           if ( result.length > 0){
+               res.json(result);
+           }else{
+               res.send('no result');
+           }
+       });
+    
+
+
+    });
+
+    app.get('/preciosLuzMadiaMes',(req,res) => {
+        res.send('precios luz Mes');
+    });
+
+    //----------------- Datos usuario -------------------//
+
 
 
 
